c14424f2f18e91badb53f90a5fb0346e7b98f290
kenechukwuJosiah <kennysuccesskay@gmail.com>
2021-08-11 15:59:19 +0100

    Added import data file that imorts all our datas to the database. -m Made a few changes in the sever file

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const fs = require('fs');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const Tour = require('../../model/tourModel');
+
+/*
+ const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+*/
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('DB connection successful');
+  })
+  .catch(() => {
+    console.log('DB ERROR: Could not connect');
+  });
+
+// Read json file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// Import data into database
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully Loaded');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+//Delete all data from database
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Deleted successfully');
+  } catch (error) {
+    console.log(error);
+    process.exit();
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+/*
+ const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+*/
+
 mongoose
   .connect(process.env.DATABASE_LOCAL, {
     useNewUrlParser: true,
